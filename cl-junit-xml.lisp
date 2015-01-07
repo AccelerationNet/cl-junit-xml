3140e9c0a5010840d485d5d86519b9689c0d9706
Ryan Davis <ryan@acceleration.net>
2015-01-06 19:50:00 -0500

    fix some load-order issues
    refs Programming:#912 (.25)

--- cl-junit-xml.lisp
+++ cl-junit-xml.lisp
@@ -4,6 +4,22 @@
 
 ;;; "cl-junit-xml" goes here. Hacks and glory await!
 
+(defclass junit-xml ()
+  ((testsuites :accessor testsuites :initarg :testsuites)))
+
+(defclass junit-testsuite ()
+  ((testcases :accessor testcases :initarg :testcases)
+   (name :reader name :initarg :name)
+   (timestamp :reader timestamp :initarg :timestamp)))
+
+(defclass junit-testcase ()
+  ((name :reader name :initarg :name)
+   (duration :reader duration :initarg :duration)
+   (class-name :reader class-name :initarg :class-name)
+   (error-text :reader error-text :initarg :error-text)
+   (failure-text :reader failure-text :initarg :failure-text)))
+
+
 (defgeneric write-xml (junit-xml sink &key pretty-p)
   (:documentation "write the junit to the given sink (string, pathname, T, nil)")
   (:method (junit-xml (sink string) &key pretty-p)
@@ -17,7 +33,7 @@
       (%write-xml junit-xml
                   (cxml:make-octet-stream-sink
                    stream :encoding :utf-8 :indentation (when pretty-p 2))))
-    sink)
+    (truename sink))
   (:method (junit-xml (sink null) &key pretty-p)
     (with-output-to-string (s)
       (%write-xml junit-xml
@@ -74,18 +90,3 @@
   (:method ((p junit-testsuite) (c junit-testcase))
     (push c (testcases p))
     c))
-
-(defclass junit-xml ()
-  ((testsuites :accessor testsuites :initarg :testsuites)))
-
-(defclass junit-testsuite ()
-  ((testcases :accessor testcases :initarg :testcases)
-   (name :reader name :initarg :name)
-   (timestamp :reader timestamp :initarg :timestamp)))
-
-(defclass junit-testcase ()
-  ((name :reader name :initarg :name)
-   (duration :reader duration :initarg :duration)
-   (class-name :reader class-name :initarg :class-name)
-   (error-text :reader error-text :initarg :error-text)
-   (failure-text :reader failure-text :initarg :failure-text)))
